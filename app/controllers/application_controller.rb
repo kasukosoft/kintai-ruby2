dfd05e0a882439d0857686f3c20b7245c28820d7
kasuko <kasukosoft@gmail.com>
2019-06-22 13:35:46 +0000

    Finish Attendances Show Page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   
   include SessionsHelper
+  include AttendancesHelper
   
-  def hello
-  render html: "Hello, World!"
-  end
 end
